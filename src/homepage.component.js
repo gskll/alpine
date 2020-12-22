e6140fe34a9c8ddca2a1b417b332fb006e4243c3
Andrew <gskll@protonmail.com>
2020-12-22 17:38:37 +0100

    style: add homepage styles

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./homepage.styles.scss";
+
 const HomePage = () => {
   return (
     <div className="homepage">
@@ -7,31 +9,31 @@ const HomePage = () => {
         <div className="menu-item">
           <div className="content">
             <h1 className="title">HATS</h1>
-            <span class="subtitle">SHOP NOW</span>
+            <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">JACKETS</h1>
-            <span class="subtitle">SHOP NOW</span>
+            <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">SHOES</h1>
-            <span class="subtitle">SHOP NOW</span>
+            <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">WOMEN</h1>
-            <span class="subtitle">SHOP NOW</span>
+            <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">MEN</h1>
-            <span class="subtitle">SHOP NOW</span>
+            <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
       </div>
