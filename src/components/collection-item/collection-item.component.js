669878980c4bf3ee0373de98f31374038fb89e51
Andrew <gskll@protonmail.com>
2021-01-04 23:11:00 +0100

    refactor: convert collection-item component to styled-component

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -2,24 +2,30 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { addItem } from "../../redux/cart/cart.actions";
-import CustomButton from "../custom-button/custom-button.component";
 
-import "./collection-item.styles.scss";
+import {
+  AddToCartButton,
+  BackgroundImage,
+  CollectionFooterContainer,
+  CollectionItemContainer,
+  ItemNameContainer,
+  ItemPriceContainer,
+} from "./collection-item.styles";
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
   return (
-    <div className="collection-item">
-      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+    <CollectionItemContainer>
+      <BackgroundImage imageUrl={imageUrl} className="image" />
+      <CollectionFooterContainer>
+        <ItemNameContainer>{name}</ItemNameContainer>
+        <ItemPriceContainer>{price}</ItemPriceContainer>
+      </CollectionFooterContainer>
+      <AddToCartButton onClick={() => addItem(item)} inverted>
         ADD TO CART
-      </CustomButton>
-    </div>
+      </AddToCartButton>
+    </CollectionItemContainer>
   );
 };
 
