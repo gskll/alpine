7a1971b77e62f5359d705a0f1cd18388abebe192
Andrew <gskll@protonmail.com>
2021-01-04 11:08:41 +0100

    refactor: move shop collections into redux store

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,22 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import SHOP_DATA from "../../data/shop.data";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { collections: SHOP_DATA };
-  }
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-  render() {
-    return (
-      <div className="shop-page">
-        {this.state.collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
