818ef6c917388a0ded7c1ec48c91b34c1fc9210e
Andrew <gskll@protonmail.com>
2021-02-10 13:09:01 +0100

    refactor: move shop data fetching from thunk to saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,9 +1,37 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from "../../firebase/firebase.utils";
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+} from "./shop.actions";
 
 import ShopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync() {
   yield console.log("i am fired");
+
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+
+  // collectionRef
+  //   .get()
+  //   .then((snapshot) => {
+  //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+  //     dispatch(fetchCollectionsSuccess(collectionsMap));
+  //   })
+  //   .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {
